f502492656fdc9ce683850e763087662ba5836d8
JP. Pacheco <48804915+jeypnet@users.noreply.github.com>
2022-09-21 15:38:53 -0500

    Imported from Quest

--- src/components/types.ts
+++ src/components/types.ts
@@ -52,3 +52,45 @@ export interface Dashboard1Props {
 };
 
 
+
+export interface ProductCard1Props {
+  className?: any;
+  prop1: any;
+};
+
+export interface ProductCard1Props {
+  className?: any;
+};
+
+
+
+
+
+
+export interface Dashboard1Props {
+  className?: any;
+  prop1: any;
+};
+
+
+
+export interface ProductCard1Props {
+  className?: any;
+  prop1: any;
+};
+
+export interface ProductCard1Props {
+  className?: any;
+};
+
+
+
+
+
+
+export interface Dashboard1Props {
+  className?: any;
+  prop1: any;
+};
+
+
