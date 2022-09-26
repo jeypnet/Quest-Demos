72bf106213b4df6c6e5d78e99b63451fed9e606c
JP. Pacheco <48804915+jeypnet@users.noreply.github.com>
2022-09-26 13:53:16 -0500

    Imported from Quest

--- src/components/ProductCard1.tsx
+++ src/components/ProductCard1.tsx
@@ -12,7 +12,7 @@
 *
 **********************************************************************/
 
-import { Button } from '@mui/material';
+import { Tooltip, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { ProductCard1Props } from './types';
 import useProductCard1 from './useProductCard1';
@@ -119,9 +119,11 @@ function ProductCard1(props: ProductCard1Props): JSX.Element {
       <About >
         <AboutProduct >
           <Text >
-            <Product >
-              {`Your Text Goes Here`}
-                </Product>
+            <Tooltip arrow={true} placement={"top"}  title={"Product Text"}>
+              <Product >
+                {`Your Text Goes Here`}
+                  </Product>
+            </Tooltip>
             <Price >
               {data.price}
                 </Price>
